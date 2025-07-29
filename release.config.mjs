98b2d386e4a7faacbd9120fa8b21272d8a39eade
张易 <2522248305@qq.com>
2025-07-29 15:58:19 +0800

    feat(workflow)：@semantic-release/github

---
+++ release.config.mjs
@@ -0,0 +1,13 @@
+const config = {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/github',
+    '@semantic-release/npm',
+    '@semantic-release/git',
+  ],
+};
+
+export default config;

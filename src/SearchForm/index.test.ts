c22157c4962d5a99bda7af0306922a1da8aa6102
张易 <2522248305@qq.com>
2025-08-25 14:13:05 +0800

    test：example test to utils

--- src/SearchForm/index.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@rstest/core';
-import { SearchForm } from '.';
-
-test('should npm SearchForm', () => {
-  expect(SearchForm).toBe('hi');
-});
--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -1,11 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import { expect, test } from 'vitest';
-import { Button } from '../src/Button';
+import { expect, test } from '@rstest/core';
+// import { SearchForm } from '../src/SearchForm/index';
+import { formatTime } from '../src/utils/format';
 
-test('The button should have correct background color', async () => {
-  render(<Button backgroundColor="#ccc" label="Demo Button" />);
-  const button = screen.getByText('Demo Button');
-  expect(button).toHaveStyle({
-    backgroundColor: '#ccc',
-  });
+test('should test formatTime utils', async () => {
+  // render(<SearchForm backgroundColor="#ccc" label="Demo Button" />);
+  // const button = screen.getByText('Demo Button');
+  expect(formatTime('2025-08-21 00:00:00', 'Y-M-D')).toBe('2025-08-21');
 });
+
+// import { render, screen } from '@testing-library/react';
+// import { expect, test } from 'vitest';
+// import { Button } from '../src/Button';
+
+// test('The button should have correct background color', async () => {
+//   render(<Button backgroundColor="#ccc" label="Demo Button" />);
+//   const button = screen.getByText('Demo Button');
+//   expect(button).toHaveStyle({
+//     backgroundColor: '#ccc',
+//   });
+// });

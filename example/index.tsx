d9d78daaaef1c8acc913fc77ec295d935c8256b7
张易 <2522248305@qq.com>
2025-12-29 16:46:55 +0800

    chore：temporarily ignore the missing type declaration(react-dom/client)

--- example/index.tsx
+++ example/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// @ts-ignore  // 临时忽略类型声明缺失，等待官方 @types/react-dom 支持 React 19
 import { createRoot } from 'react-dom/client';
 import App from './App';
 // 由于 React 19 调整了 react-dom 的导出方式，导致 antd 无法直接使用 ReactDOM.render 方法
